01240b778ca88fe19c3ba9b4eb396856958331a5
JackCokebb <reo2726@gmail.com>
2022-09-19 12:54:40 +0900

    dag test port change3

--- airflow-DAGS/pull_raw2aug_dag.py
+++ airflow-DAGS/pull_raw2aug_dag.py
@@ -23,8 +23,7 @@ from sqlalchemy.sql import text
 #pull raw data in the cloud and run the aug module. Then save the aug data files in the local.
 def pull_mds_gan():
     consumer = KafkaConsumer('raw.coops2022.molding_data',
-            group_id='my-group',
-            bootstrap_servers=['kafka-clust-kafka-persis-d198b-11683092-d3d89e335b84.kr.lb.naverncp.com:9092'],
+            bootstrap_servers=['kafka-clust-kafka-persis-d198b-11683092-d3d89e335b84.kr.lb.naverncp.com:9094'],
             auto_offset_reset='earliest'
             )
     consumer.poll(timeout_ms=1000, max_records=2000)
