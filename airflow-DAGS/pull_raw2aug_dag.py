aaf79a3c3b87e8aafa9ffce0fd2cb81a6fab500c
leejunseok <wt358@naver.com>
2022-09-21 15:45:11 +0900

    df drop columns

--- airflow-DAGS/pull_raw2aug_dag.py
+++ airflow-DAGS/pull_raw2aug_dag.py
@@ -78,6 +78,7 @@ def pull_mds_gan():
             'NGmark',
             '_id',},inplace=True)
     print(df.head())
+    print(df.columns())
     moldset_labeled_9000R=df[df.Additional_Info_1=='09520 9000R']
     moldset_labeled_9000R=moldset_labeled_9000R.reset_index(drop=True)
     print(moldset_labeled_9000R.head())
