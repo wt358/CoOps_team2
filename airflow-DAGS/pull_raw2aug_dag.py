322493e1a9ef17b8d7619504fb7182d0297c42b1
leejunseok <wt358@naver.com>
2022-09-21 13:34:15 +0900

    json data extract3

--- airflow-DAGS/pull_raw2aug_dag.py
+++ airflow-DAGS/pull_raw2aug_dag.py
@@ -43,7 +43,7 @@ def pull_mds_gan():
         message = message.value
         print(type(message['payload']))
         print(message['payload'])
-        print(json.loads(message['payload'])['fullDocument'])
+        print(loads(message['payload'])['fullDocument'])
         l.append(message['payload']['fullDocument'])
     df = pd.DataFrame(l)
     
