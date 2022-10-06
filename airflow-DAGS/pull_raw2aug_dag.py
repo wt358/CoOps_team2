51c9853e024ab19bbaf8b4fdf4ea95beb8b09866
leejunseok <wt358@naver.com>
2022-10-06 16:51:04 +0900

    OC_SVM pull aug data to module

--- airflow-DAGS/pull_raw2aug_dag.py
+++ airflow-DAGS/pull_raw2aug_dag.py
@@ -426,6 +426,7 @@ def pull_mds_gan():
     data=gen_df.to_dict('records')
     # 아래 부분은 테스트 할 때 매번 다른 oid로 데이터가 쌓이는 것을 막기 위함
     try:
+        isData = collection_aug.find_one()
         result = collection_aug.insert_many(data,ordered=False)
     except Exception as e:
         print("mongo connection failed", e)
