48aa0ab5702e47809e25c7ba5edfb20a641963ed
leejunseok <wt358@naver.com>
2022-10-14 13:32:49 +0900

    retrieve oc_svm model

--- airflow-DAGS/pull_raw2aug_dag.py
+++ airflow-DAGS/pull_raw2aug_dag.py
@@ -645,9 +645,9 @@ def oc_svm():
     model_fpath = f'{model_name}.joblib'
     joblib.dump(model, model_fpath)
     
-    result = collection_model.find({"filename": model_name }).sort('uploadDate', -1)
+    result = collection_model.find({"model_name": model_name }).sort('uploadDate', -1)
 
-    print(result[0]['_id'])
+    print(result)
     ''' 
     if result.count():
         model_name = str(result[0]['_id'])
