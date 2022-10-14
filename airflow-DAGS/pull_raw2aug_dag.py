44917ce5e8d0e36e5bc9fea1cb80eff2de80498b
leejunseok <wt358@naver.com>
2022-10-14 14:30:30 +0900

    retrieve oc_svm model

--- airflow-DAGS/pull_raw2aug_dag.py
+++ airflow-DAGS/pull_raw2aug_dag.py
@@ -653,7 +653,7 @@ def oc_svm():
     print(result[0])
     print(result[0]['model_name'])
     
-    if len(result)!=0:
+    if len(list(result))!=0:
         model_name = str(result[0]['model_name'])
     
 
