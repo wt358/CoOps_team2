bd2d3f8689b5784376045352423d4a3d22766cc6
leejunseok <wt358@naver.com>
2022-10-19 17:15:45 +0900

    k8s operator test

---
+++ airflow-DAGS/k8sop_test.py
@@ -0,0 +1,14 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+
+k = KubernetesPodOperator(
+        name="hello-dry-run",
+        image="debian",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        task_id="dry_run_demo",
+        do_xcom_push=True,
+)
+
+k.dry_run()
