568c462c6b189e42b474f99fc9c430665ee40f88
leejunseok <wt358@naver.com>
2022-10-19 17:28:28 +0900

    k8s operator test

--- airflow-DAGS/k8sop_test.py
+++ airflow-DAGS/k8sop_test.py
@@ -1,14 +1,63 @@
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from kubernetes.client import models as k8s
+from datetime import datetime, timedelta
 
-k = KubernetesPodOperator(
-        name="hello-dry-run",
-        image="debian",
-        cmds=["bash", "-cx"],
-        arguments=["echo", "10"],
-        labels={"foo": "bar"},
-        task_id="dry_run_demo",
-        do_xcom_push=True,
+from kubernetes.client import models as k8s
+from airflow.models import DAG, Variable
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.kubernetes.secret import Secret
+from airflow.kubernetes.pod import Resources
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (KubernetesPodOperator,)
+
+dag_id = 'kubernetes-dag'
+
+task_default_args = {
+        'owner': 'coops2',
+        'retries': 0,
+        'retry_delay': timedelta(minutes=5),
+        'depends_on_past': True,
+        'execution_timeout': timedelta(hours=1)
+}
+
+dag = DAG(
+        dag_id=dag_id,
+        description='kubernetes pod operator',
+        default_args=task_default_args,
+        schedule_interval='5 16 * * *',
+        max_active_runs=1
+)
+'''
+env = Secret(
+        'env',
+        'TEST',
+        'test_env',
+        'TEST',
 )
 
-k.dry_run()
+pod_resources = Resources()
+pod_resources.request_cpu = '1000m'
+pod_resources.request_memory = '2048Mi'
+pod_resources.limit_cpu = '2000m'
+pod_resources.limit_memory = '4096Mi'
+
+
+configmaps = [
+        k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='secret')),
+        ]
+'''
+
+start = DummyOperator(task_id="start", dag=dag)
+
+run = KubernetesPodOperator(
+        task_id="kubernetespodoperator",
+        namespace='development',
+        image='test/image',
+        name="job",
+        is_delete_operator_pod=True,
+        get_logs=True,
+        resources=pod_resources,
+        env_from=configmaps,
+        dag=dag,
+        )
+
+start >> run
+
+
