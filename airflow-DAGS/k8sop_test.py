f0571bb615d4b8e02983282c881f3f5ccf838309
leejunseok <wt358@naver.com>
2022-10-19 18:15:33 +0900

    k8soperator test

--- airflow-DAGS/k8sop_test.py
+++ airflow-DAGS/k8sop_test.py
@@ -26,26 +26,33 @@ dag = DAG(
         schedule_interval=timedelta(days=1),
         #max_active_runs=1
 )
-env = Secret(
-        'env',
-        'TEST',
-        'test_env',
-        'TEST',
-)
 
-configmaps = [
-        k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='secret')),
-        ]
+env_from = [
+        k8s.V1EnvFromSource(
+            # configmap fields를  key-value 형태의 dict 타입으로 전달한다. 
+            config_map_ref=k8s.V1ConfigMapEnvSource(name="airflow-cluster-pod-template"),
+            # secret fields를  key-value 형태의 dict 타입으로 전달한다.
+            secret_ref=k8s.V1SecretEnvSource(name="regcred")),
+]
+
+
+
+
+
+
 
 start = DummyOperator(task_id="start", dag=dag)
 
 run = KubernetesPodOperator(
         task_id="kubernetespodoperator",
         namespace='airflow-cluster',
+        
+        
         image='model-image.kr.ncr.ntruss.com/airflow-py:0.7',
-        image_pull_secrets=[k8s.V1LocalObjectReference('regcred')],
+        image_pull_policy="Always",
+        #image_pull_secrets=[k8s.V1LocalObjectReference('regcred')],
         name="job",
-        #is_delete_operator_pod=True,
+        is_delete_operator_pod=True,
         get_logs=True,
         dag=dag,
         )
