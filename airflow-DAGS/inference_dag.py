0c81cccbf7a91068e41f7563a4ad538d0ea956a4
leejunseok <wt358@naver.com>
2022-10-20 21:15:41 +0900

    inference model

--- airflow-DAGS/inference_dag.py
+++ airflow-DAGS/inference_dag.py
@@ -1,22 +1,74 @@
 from datetime import timedelta
+from datetime import datetime
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.models.variable import Variable
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import MDS
+from sklearn.metrics import precision_score, recall_score, f1_score,accuracy_score, classification_report, plot_confusion_matrix, confusion_matrix
 
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM 
+from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score
+
+from IPython.display import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+from bson import ObjectId
+
+import gridfs
+import io
+
+from gridfs import GridFS
+
+
+
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline
+
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply, Concatenate, Dropout, LSTM, TimeDistributed, RepeatVector
+from tensorflow.keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D, LeakyReLU
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.initializers import RandomNormal
+import tensorflow.keras.backend as K
+from tensorflow.keras import regularizers
+from tensorflow.python.client import device_lib
+from sklearn.utils import shuffle
+import tensorflow as tf
+
+import joblib
+
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 import influxdb_client
 import csv
-from pymongo import MongoClient
 import pandas as pd
 import os
+import time
+import numpy as np
+from collections import Counter
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+from pymongo import MongoClient
 
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 
+from json import loads
+import random as rn
 # define funcs
 def model_inference():
     #data consumer
