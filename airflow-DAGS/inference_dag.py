74e4190228b8334d07f99900074b1952f930313d
leejunseok <wt358@naver.com>
2022-10-21 17:29:43 +0900

    model inference test

--- airflow-DAGS/inference_dag.py
+++ airflow-DAGS/inference_dag.py
@@ -86,6 +86,39 @@ tf.random.set_seed(RANDOM_SEED)
 rn.seed(10)
 
 tf.random.set_seed(10)
+class ModelSingleton(type):
+   """
+   Metaclass that creates a Singleton base type when called.
+   """
+   _mongo_id = {}
+   def __call__(cls, *args, **kwargs):
+       mongo_id = kwargs['mongo_id']
+       if mongo_id not in cls._mongo_id:
+           print('Adding model into ModelSingleton')
+           cls._mongo_id[mongo_id] = super(ModelSingleton, cls).__call__(*args, **kwargs)
+       return cls._mongo_id[mongo_id]
+
+class LoadModel(metaclass=ModelSingleton):
+   def __init__(self, *args, **kwargs):
+       print(kwargs)
+       self.mongo_id = kwargs['mongo_id']
+       self.clf = self.load_model()
+   def load_model(self):
+       print('loading model')
+
+       mongoClient = MongoClient()
+       host = Variable.get("MONGO_URL_SECRET")
+       client = MongoClient(host)
+
+       db_model = client['coops2022_model']
+       fs = gridfs.GridFS(db_model)
+       print(self.mongo_id)
+       f = fs.find({"_id": ObjectId(self.mongo_id)}).next()
+       print(f)
+
+       with open(f'{f.model_name}.joblib', 'wb') as outfile:
+           outfile.write(f.read())
+       return joblib.load(f'{f.model_name}.joblib')
 
 # define funcs
 def model_inference():
