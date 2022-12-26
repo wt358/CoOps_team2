dd27209778d8f0f5fde7aed10a33bf66efeb2801
leejunseok <wt358@naver.com>
2022-12-26 13:58:00 +0900

    code redesign & add time series column in gan

---
+++ airflow/airflow-DAGS/pyfile/loadmodel.py
@@ -0,0 +1,45 @@
+
+import gridfs
+import os
+from gridfs import GridFS
+import joblib
+from bson import ObjectId
+from pymongo import MongoClient
+
+class ModelSingleton(type):
+   """
+   Metaclass that creates a Singleton base type when called.
+   """
+   _mongo_id = {}
+   def __call__(cls, *args, **kwargs):
+       mongo_id = kwargs['mongo_id']
+       if mongo_id not in cls._mongo_id:
+           print('Adding model into ModelSingleton')
+           cls._mongo_id[mongo_id] = super(ModelSingleton, cls).__call__(*args, **kwargs)
+       return cls._mongo_id[mongo_id]
+   
+class LoadModel(ModelSingleton):
+   def __init__(self, *args, **kwargs):
+       print(kwargs)
+       self.mongo_id = kwargs['mongo_id']
+       self.clf = self.load_model()
+   def load_model(self):
+       print('loading model')
+
+       mongoClient = MongoClient()
+       #host = Variable.get("MONGO_URL_SECRET")
+       host = os.environ['MONGO_URL_SECRET'] 
+       client = MongoClient(host)
+
+       db_model = client['coops2022_model']
+       fs = gridfs.GridFS(db_model)
+       print(self.mongo_id)
+       f = fs.find({"_id": ObjectId(self.mongo_id)}).next()
+       print(f)
+       client.close()
+       with open(f'{f.model_name}.joblib', 'wb') as outfile:
+           outfile.write(f.read())
+       return joblib.load(f'{f.model_name}.joblib')
+   
+   
+   
---
+++ airflow/airflow-DAGS/pyfile/preprocess.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import time
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import MDS
+
+from sklearn.cluster import DBSCAN
+
+def customize(dataframe,mds_matrix):
+    answer=pd.DataFrame(np.zeros(len(mds_matrix)))
+    answer.rename(columns = {0:'Class'},inplace=True)## 일단 전부 양품으로 간주하느 데이터프레임 생성
+    for i in range(1,10):
+        for j in range(1,10):
+            dbscan=DBSCAN(eps = i,min_samples=j)
+            clusters_mds = pd.DataFrame(dbscan.fit_predict(mds_matrix))
+            clusters_mds.rename(columns = {0:'Class'},inplace=True)
+            if clusters_mds.value_counts().count()==2:
+                if len(clusters_mds.loc[clusters_mds['Class'] == -1]) >  len(answer.loc[answer['Class'] == -1]): 
+                    answer=clusters_mds
+    dataframe.reset_index(inplace=True,drop=True)          
+    result = pd.DataFrame(pd.concat([dataframe,answer], axis = 1))       
+    return result
+
+def IQR(section):
+    
+    section.iloc[:,6:17]=section.iloc[:,6:17].apply(pd.to_numeric,errors='coerce')
+    for i in range(6,17):
+        level_1q = section.iloc[:,i].quantile(0.025)
+        level_3q = section.iloc[:,i].quantile(0.975)
+        IQR = level_3q - level_1q
+        rev_range = 1.5 # 제거 범위 조절 변수
+        section = section[(section.iloc[:,i] <= level_3q + (rev_range * IQR)) & (section.iloc[:,i] >= level_1q - (rev_range * IQR))] ## sectiond에 저장된 데이터 프레임의 이상치 제거 작업
+    return section
+
+def MDS_molding(pds):
+    list1=[]## 불량여부가 라벨링된 구간별 데이터 프레임을 저장할 리스트
+
+    for i in range(len(pds)):
+        start_time = time.time()
+        print('%d 번째' %i)
+        pds[i]['TimeStamp']=pd.to_datetime(pds[i]['TimeStamp']).astype('int64')/10**9
+        dataframe=pds[i].iloc[:,5:]
+        if len(pds[i])>=30:
+            std = StandardScaler().fit_transform(pds[i].iloc[:,5:]) ## 정규화 진행
+            end_time = time.time()
+            print('    if std 코드 실행 시간: %20ds' % (end_time - start_time))
+            mds_results = MDS(n_components=2).fit_transform(std) ## mds차원축소결과 저장(시간이 좀 많이 소요됨)
+            end_time = time.time()
+            print('    if mds 코드 실행 시간: %20ds' % (end_time - start_time))
+            mds_results=pd.DataFrame(mds_results) ##dataframe 형태로 저장 
+            end_time = time.time()
+            print('    if df 코드 실행 시간: %20ds' % (end_time - start_time))
+            list1.append(customize(dataframe,mds_results))## 구간별 라벨링 데이터 프레임을 리스트에 저장
+            end_time = time.time()
+            print('    if 코드 실행 시간: %20ds' % (end_time - start_time))
+        else :
+            answer=pd.DataFrame(np.zeros(len(pds[i]))) 
+            answer.rename(columns = {0:'Class'},inplace=True) 
+            dataframe.reset_index(inplace=True,drop=True)     
+            result = pd.DataFrame(pd.concat([pds[i].iloc[:,5:],answer], axis = 1))
+            list1.append(result)
+            end_time = time.time()
+            print('    else 코드 실행 시간: %20ds' % (end_time - start_time))
+    df_all=pd.concat(list1, ignore_index=True)
+    
+    return df_all
