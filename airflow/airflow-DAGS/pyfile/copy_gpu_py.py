a8aac0949424707609a6c47e6b215e0a1d4ddcdf
leejunseok <wt358@naver.com>
2023-01-05 17:46:37 +0900

    cursor sort

--- airflow/airflow-DAGS/pyfile/copy_gpu_py.py
+++ airflow/airflow-DAGS/pyfile/copy_gpu_py.py
@@ -353,15 +353,15 @@ def oc_svm():
     model_fpath = f'{model_name}.joblib'
     result = collection_model.find({"model_name": model_name}).sort([("uploadDate", -1)])
     print(result)
-    cnt=result.limit
+    cnt=list(result.clone())
     print(result[0])
-    print(result[cnt])
-    if len(list(result.clone()))==0:
+    print(result[cnt-1])
+    if len(cnt)==0:
         print("empty")
         model = OneClassSVM(kernel = 'rbf', gamma = 0.001, nu = 0.04).fit(target_columns)
     else:
         print("not empty")
-        file_id = str(result[cnt]['file_id'])
+        file_id = str(result[cnt-1]['file_id'])
 
         model = LoadModel(mongo_id=file_id).clf
     joblib.dump(model, model_fpath)
@@ -549,16 +549,16 @@ def lstm_autoencoder():
         model_fpath = f'{model_name}.joblib'
         result = collection_model.find({"model_name": model_name}).sort([("uploadDate", -1)])
         print(result)
-        cnt=result.limit
+        cnt=list(result.clone())
         print(cnt)
         print(result[0])
-        print(result[cnt])
-        if len(list(result.clone()))==0:
+        print(result[cnt-1])
+        if len(cnt)==0:
             print("empty")
             model = autoencoder_model(X_train)
         else:
             print("not empty")
-            file_id = str(result[cnt]['file_id'])
+            file_id = str(result[cnt-1]['file_id'])
             
             model = LoadModel(mongo_id=file_id).clf
         
