7b34992a1bfe0d1086cd7d29fde158ff153c7cd5
leejunseok <wt358@naver.com>
2023-01-09 17:31:47 +0900

    logger module

--- airflow/airflow-DAGS/pyfile/logger.py
+++ airflow/airflow-DAGS/pyfile/logger.py
@@ -23,6 +23,7 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
 # log를 파일에 출력
+os.makedirs('./log/{}.txt'.format(td), exist_ok=True)
 file_handler = logging.FileHandler('./log/{}.txt'.format(td))
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
