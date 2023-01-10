4abd05f22c991e5ca68b8d359624cf545b75b2fd
leejunseok <wt358@naver.com>
2023-01-10 16:52:11 +0900

    df strring columns too int

--- airflow/airflow-DAGS/pyfile/preprocess.py
+++ airflow/airflow-DAGS/pyfile/preprocess.py
@@ -229,6 +229,10 @@ def outlier_iqr(df):
     scaled = []
     q = [0.05, 0.95]
     scaler= MinMaxScaler()
+    print(df.columns)
+    print(df.dtypes)
+    df.iloc[:,1:]=df.iloc[:,1:].apply(pd.to_numeric,errors='coerce')
+    print(df.dtypes)
     for i in df.columns[1:]:
         lb, ub = df[i].quantile(q).tolist()
         samp = df[i].map(lambda x: None if (x < lb) or (x > ub) else x)
