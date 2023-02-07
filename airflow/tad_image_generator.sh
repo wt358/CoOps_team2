c2567774aaf853d93e6377e35e464fcdd90c3a7d
leejunseok <wt358@naver.com>
2023-02-07 15:58:28 +0900

    tad image updated with venv

--- airflow/tad_image_generator.sh
+++ airflow/tad_image_generator.sh
@@ -4,8 +4,8 @@ TAG=$1
 message=$2
 
 docker images
-docker build -t wcu5i9i6.kr.private-ncr.ntruss.com/cuda:$TAG -f gpu-Dockerfile .
-docker push wcu5i9i6.kr.private-ncr.ntruss.com/cuda:$TAG
+docker build -t wcu5i9i6.kr.private-ncr.ntruss.com/tad:$TAG -f tad-Dockerfile .
+docker push wcu5i9i6.kr.private-ncr.ntruss.com/tad:$TAG
 
 git add .
 git commit -m "$2"
