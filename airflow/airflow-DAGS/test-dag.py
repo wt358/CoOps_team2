46f44436dcc06ed746f1c1fba1215692c34da342
leejunseok <wt358@naver.com>
2023-02-17 15:41:59 +0900

    test the dag scheduling

--- airflow/airflow-DAGS/test-dag.py
+++ airflow/airflow-DAGS/test-dag.py
@@ -1,5 +1,6 @@
 """Example DAG demonstrating the usage of the PythonOperator."""
 import time
+from datetime import timedelta
 from pprint import pprint
 
 from airflow import DAG
@@ -13,7 +14,7 @@ args = {
 with DAG(
     dag_id='python_operator',
     default_args=args,
-    schedule_interval=None,
+    schedule_interval=timedelta(days=1),
     start_date=days_ago(1),
     tags=['example'],
 ) as dag:
@@ -48,28 +49,28 @@ with DAG(
     # [END howto_operator_python_kwargs]
 
     # [START howto_operator_python_venv]
-    def callable_virtualenv():
-        """
-        Example function that will be performed in a virtual environment.
-        Importing at the module level ensures that it will not attempt to import the
-        library before it is installed.
-        """
-        from time import sleep
+    # def callable_virtualenv():
+    #     """
+    #     Example function that will be performed in a virtual environment.
+    #     Importing at the module level ensures that it will not attempt to import the
+    #     library before it is installed.
+    #     """
+    #     from time import sleep
 
-        from colorama import Back, Fore, Style
+    #     from colorama import Back, Fore, Style
 
-        print(Fore.RED + 'some red text')
-        print(Back.GREEN + 'and with a green background')
-        print(Style.DIM + 'and in dim text')
-        print(Style.RESET_ALL)
-        for _ in range(10):
-            print(Style.DIM + 'Please wait...', flush=True)
-            sleep(10)
-        print('Finished')
+    #     print(Fore.RED + 'some red text')
+    #     print(Back.GREEN + 'and with a green background')
+    #     print(Style.DIM + 'and in dim text')
+    #     print(Style.RESET_ALL)
+    #     for _ in range(10):
+    #         print(Style.DIM + 'Please wait...', flush=True)
+    #         sleep(10)
+    #     print('Finished')
 
-    virtualenv_task = PythonVirtualenvOperator(
-        task_id="virtualenv_python",
-        python_callable=callable_virtualenv,
-        requirements=["colorama==0.4.0"],
-        system_site_packages=False,
-    )
+    # virtualenv_task = PythonVirtualenvOperator(
+    #     task_id="virtualenv_python",
+    #     python_callable=callable_virtualenv,
+    #     requirements=["colorama==0.4.0"],
+    #     system_site_packages=False,
+    # )
