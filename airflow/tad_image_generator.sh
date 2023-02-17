1b39b96301318b5d9a4f799bcaa77ccee7e922d2
leejunseok <wt358@naver.com>
2023-02-17 15:21:05 +0900

    git clone dockerfile

--- airflow/tad_image_generator.sh
+++ airflow/tad_image_generator.sh
@@ -6,6 +6,7 @@ message=$2
 docker images
 docker build -t wcu5i9i6.kr.private-ncr.ntruss.com/tad:$TAG -f tad-Dockerfile .
 docker push wcu5i9i6.kr.private-ncr.ntruss.com/tad:$TAG
+docker rmi wcu5i9i6.kr.private-ncr.ntruss.com/tad:$TAG
 
 git add .
 git commit -m "$2"
--- airflow/test.sh
+++ airflow/test.sh
@@ -6,6 +6,7 @@ message=$2
 docker images
 docker build -t wcu5i9i6.kr.private-ncr.ntruss.com/cuda:$TAG -f gpu-Dockerfile .
 docker push wcu5i9i6.kr.private-ncr.ntruss.com/cuda:$TAG
+docker rmi wcu5i9i6.kr.private-ncr.ntruss.com/cuda:$TAG
 
 git add .
 git commit -m "$2"
