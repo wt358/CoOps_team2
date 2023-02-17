6a3eb15e7b393a02e837752fd7b4455bda741cf0
leejunseok <wt358@naver.com>
2023-02-17 16:06:59 +0900

    ds k8s test

--- airflow/airflow-DAGS/test-dag.py
+++ airflow/airflow-DAGS/test-dag.py
@@ -71,17 +71,17 @@ with DAG(
         time.sleep(random_base)
 
     # Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
-    for i in range(2):
-        task = PythonOperator(
-            task_id='sleep_for_' + str(i),
-            python_callable=my_sleeping_function,
-            op_kwargs={'random_base': float(i) / 10},
-            owner="coops2",
-            retries=0,
-            retry_delay=timedelta(minutes=1),
-        )
+    # for i in range(2):
+    #     task = PythonOperator(
+    #         task_id='sleep_for_' + str(i),
+    #         python_callable=my_sleeping_function,
+    #         op_kwargs={'random_base': float(i) / 10},
+    #         owner="coops2",
+    #         retries=0,
+    #         retry_delay=timedelta(minutes=1),
+    #     )
 
-        run_this >> task
+        run_this >>kubenetes_template_ex
     # [END howto_operator_python_kwargs]
 
     # [START howto_operator_python_venv]
