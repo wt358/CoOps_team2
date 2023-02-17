da78dc6a88919a1b068f1feed1bc80d95c528964
leejunseok <wt358@naver.com>
2023-02-17 16:06:13 +0900

    ds k8s test

--- airflow/airflow-DAGS/test-dag.py
+++ airflow/airflow-DAGS/test-dag.py
@@ -6,7 +6,9 @@ from pprint import pprint
 from airflow import DAG
 from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from airflow.utils.dates import days_ago
-
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
 args = {
     'owner': 'airflow',
 }
@@ -36,7 +38,33 @@ with DAG(
         depends_on_past=True,
     )
     # [END howto_operator_python]
-
+    kubenetes_template_ex = KubernetesPodOperator(
+        task_id="ex-kube-templates",
+        name="ex-kube-templates",
+        namespace="default",
+        image="bash",
+        # All parameters below are able to be templated with jinja -- cmds,
+        # arguments, env_vars, and config_file. For more information visit:
+        # https://airflow.apache.org/docs/apache-airflow/stable/macros-ref.html
+        # Entrypoint of the container, if not specified the Docker container's
+        # entrypoint is used. The cmds parameter is templated.
+        cmds=["echo"],
+        # DS in jinja is the execution date as YYYY-MM-DD, this docker image
+        # will echo the execution date. Arguments to the entrypoint. The docker
+        # image's CMD is used if this is not provided. The arguments parameter
+        # is templated.
+        arguments=["{{ ds }}"],
+        # The var template variable allows you to access variables defined in
+        # Airflow UI. In this case we are getting the value of my_value and
+        # setting the environment variable `MY_VALUE`. The pod will fail if
+        # `my_value` is not set in the Airflow UI.
+        env_vars={"MY_VALUE": "{{ var.value.my_value }}"},
+        # Sets the config file to a kubernetes config file specified in
+        # airflow.cfg. If the configuration file does not exist or does
+        # not provide validcredentials the pod will fail to launch. If not
+        # specified, config_file defaults to ~/.kube/config
+        config_file="{{ conf.get('core', 'kube_config') }}",
+    )
     # [START howto_operator_python_kwargs]
     def my_sleeping_function(random_base):
         """This is a function that will run within the DAG execution"""
